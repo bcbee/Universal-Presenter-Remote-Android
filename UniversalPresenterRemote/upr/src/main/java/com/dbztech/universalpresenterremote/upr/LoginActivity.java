3fb7ed62df7b722f34250d3992c61cc3fa864cd2
Brendan Boyle <brendancboyle@me.com>
2014-05-08 19:58:27 -0500

    Fixed backgrounding and added legacy instructions

--- UniversalPresenterRemote/upr/src/main/java/com/dbztech/universalpresenterremote/upr/LoginActivity.java
+++ UniversalPresenterRemote/upr/src/main/java/com/dbztech/universalpresenterremote/upr/LoginActivity.java
@@ -14,12 +14,26 @@ import java.util.Timer;
 
 public class LoginActivity extends Activity {
     public final static String TokenMessage = "com.dbztech.universalpresenterremote.upr.TOKEN";
+    public static boolean queryServer = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ServerCommunication.setupUid();
+        updateInterface();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        queryServer = false;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        queryServer = true;
         updateInterface();
     }
 
@@ -39,11 +53,15 @@ public class LoginActivity extends Activity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_about) {
-            Intent intent = new Intent(this, AboutActivity.class);
-            startActivity(intent);
+            openInstructions(null);
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void openInstructions(View view) {
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
     }
 
     /** Called when the user clicks the Send button */
@@ -54,37 +72,39 @@ public class LoginActivity extends Activity {
     }
 
     public void updateInterface() {
-        ServerCommunication.checkStatus();
-        new CountDownTimer(1500, 750) {
+        if (queryServer) {
+            ServerCommunication.checkStatus();
+            new CountDownTimer(1500, 750) {
 
-            public void onTick(long millisUntilFinished) {
-                final Button connectButton = (Button) findViewById(R.id.connectButton);
-                final TextView tokenView = (TextView) findViewById(R.id.loginToken);
-                int test = ServerCommunication.controlMode;
-                String set = Integer.toString(ServerCommunication.token);
-                switch (ServerCommunication.controlMode) {
-                    case 0:
-                        connectButton.setEnabled(false);
-                        tokenView.setText("...");
-                        break;
-                    case 1:
-                        connectButton.setEnabled(false);
-                        connectButton.setText("Waiting...");
-                        tokenView.setText(set);
-                        break;
-                    case 2:
-                        connectButton.setEnabled(true);
-                        tokenView.setText(set);
-                        connectButton.setText("Begin");
-                        break;
+                public void onTick(long millisUntilFinished) {
+                    final Button connectButton = (Button) findViewById(R.id.connectButton);
+                    final TextView tokenView = (TextView) findViewById(R.id.loginToken);
+                    int test = ServerCommunication.controlMode;
+                    String set = Integer.toString(ServerCommunication.token);
+                    switch (ServerCommunication.controlMode) {
+                        case 0:
+                            connectButton.setEnabled(false);
+                            tokenView.setText("...");
+                            break;
+                        case 1:
+                            connectButton.setEnabled(false);
+                            connectButton.setText("Waiting...");
+                            tokenView.setText(set);
+                            break;
+                        case 2:
+                            connectButton.setEnabled(true);
+                            tokenView.setText(set);
+                            connectButton.setText("Begin");
+                            break;
+                    }
                 }
-            }
 
-            public void onFinish() {
-                //Do nothing
-                updateInterface();
-            }
-        }.start();
+                public void onFinish() {
+                    //Do nothing
+                    updateInterface();
+                }
+            }.start();
+        }
     }
 
 }
